47fb3ab0298993ac73640d72123bd23b61394667
Vlad Ozhiganov <slava.northern@gmail.com>
2026-09-22 23:15:09 +0700

    Complete stage 6 dictionaries

--- catalog_analysis.py
+++ catalog_analysis.py
@@ -187,3 +187,43 @@ def top_n_by_rating(movies, n=3):
         (movie["title"], movie["rating"])
         for movie in sorted_movies[:n]
     ]
+
+
+
+# Этап 6. Словари
+
+
+
+# Фунци подсчета количества фильмов по жанрам
+def count_by_genre(movies):
+    genre_counts = {}
+
+    for movie in movies:
+        for genre in movie["genres"]:
+            genre_counts[genre] = genre_counts.get(genre, 0) + 1
+
+    return genre_counts
+
+
+# Функция показывает фильмы актера
+def actor_filmography(movies):
+    filmography = {}
+
+    for movie in movies:
+        for actor in movie["actors"]:
+            if actor not in filmography:
+                filmography[actor] = []
+
+            filmography[actor].append(movie["title"])
+
+    return filmography
+
+
+# Фильмы с рейтингом выше среднего
+average = average_rating(movies)
+
+above_average_ratings = {
+    movie["title"]: movie["rating"]
+    for movie in movies
+    if movie["rating"] > average
+}
